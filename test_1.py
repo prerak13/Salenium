626b20baaf46f7c6119d2865f4535feef1b33c27
prerak13 <choksiprerak@gmail.com>
2020-03-27 01:40:32 +0530

    files added

---
+++ test_1.py
@@ -0,0 +1,4 @@
+x = ['Carson Busses', 'Earl E. Byrd', 'Patty Cakes', 'Derri Anne Connecticut', 'Moe Dess', 'Les Toil', 'Lionel Train', 'N.VUbble ', ' Mayflower van Lines ', ' Chad A.While ', ' Dell Monte ', ' Baxter Nature ', ' Agatha L.Outtahere ',
+     ' Bunny Pellits ', ' Cheri Pitts ', ' Gill D.Lily ', ' Cole Mines ', ' Phil R.Monik ', ' L.O.Quency ', ' Connel Radd ', ' Stella Constellation ', ' Lee Derhosen ', ' Stan Dupp ', ' Hammond Ecks ', ' Fanny Farmer ']
+
+print(len(x))
